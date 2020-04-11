477443876ac1fd9aa15e55d026d5ad2e7cd69a37
michelleandersson <michelleandersson@live.com>
2020-04-11 15:30:01 +0200

    finish bg-color-changer

---
+++ bg-color-changer/bg-color-changer.js
@@ -0,0 +1,12 @@
+function bgChange() {
+    let x = Math.floor(Math.random() * 256); 
+    let y = Math.floor(Math.random() * 256); 
+    let z = Math.floor(Math.random() * 256); 
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")"; 
+    
+    console.log(bgColor); 
+
+    document.body.style.backgroundColor = bgColor; 
+}
+
+bgChange(); 
