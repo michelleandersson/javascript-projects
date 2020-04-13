fb14564733c972cbe21cc13c8b3a0356c91ae20c
michelleandersson <michelleandersson@live.com>
2020-04-13 12:06:04 +0200

    create pass message

---
+++ pass-message/pass-message.js
@@ -0,0 +1,24 @@
+function newMessage() {
+    //select the input element
+    const form = document.querySelector('#message-form')
+    //set up Submit Button 
+    form.addEventListener('submit', function(e) {
+        //prevent the form's default submission action
+        e.preventDefault()
+        //Get user's input from form 
+        const message = document.querySelector('#message'); 
+        const feedback = document.querySelector('.feedback'); 
+        const messageContent = document.querySelector('.message-content'); 
+
+        if (message.value === '') {
+            feedback.classList.add('show'); 
+            setTimeout(function() {
+                feedback.classList.remove('show')
+            }, 2000)
+        } else {
+            //change message content and clear the message input
+            messageContent.textContent = message.value; 
+            message.value = ''; 
+        }
+    })
+}
